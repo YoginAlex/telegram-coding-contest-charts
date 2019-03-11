b18f5a4af5c5a79cc26f2124d9b9640f9e874cfe
Alexey Zakharenko <yogin.alex@gmail.com>
2019-03-11 21:51:31 +0300

    fix: propTypes

--- src/components/propTypes.js
+++ src/components/propTypes.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { GRAPH_TYPES } from './enums';
 
-const graphPropType = PropTypes.arrayOf([
+const graphPropType = PropTypes.arrayOf(
   PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-]);
+);
 
 const typesPropType = PropTypes.objectOf(
-  PropTypes.oneOf([Object.values(GRAPH_TYPES)]),
+  PropTypes.oneOf([...Object.values(GRAPH_TYPES)]),
 );
 
 const arrayOfStringsPropTypes = PropTypes.objectOf(PropTypes.string);
