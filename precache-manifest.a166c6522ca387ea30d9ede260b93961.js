6a1197e7b16ab78278709365593c46646fba0fb1
Alexey Zakharenko <yogin.alex@gmail.com>
2019-03-13 22:10:00 +0300

    Updates

---
+++ precache-manifest.a166c6522ca387ea30d9ede260b93961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86a340f10af55a4c5b0",
+    "url": "/telegram-coding-contest-charts/static/js/runtime~main.2372c358.js"
+  },
+  {
+    "revision": "4c1ccd66b5317a88b884",
+    "url": "/telegram-coding-contest-charts/static/js/main.3f4f9c3d.chunk.js"
+  },
+  {
+    "revision": "f86d81de3a3753823e1c",
+    "url": "/telegram-coding-contest-charts/static/js/2.b78e9601.chunk.js"
+  },
+  {
+    "revision": "4c1ccd66b5317a88b884",
+    "url": "/telegram-coding-contest-charts/static/css/main.675f7cb1.chunk.css"
+  },
+  {
+    "revision": "f86d81de3a3753823e1c",
+    "url": "/telegram-coding-contest-charts/static/css/2.fe82003c.chunk.css"
+  },
+  {
+    "revision": "1d29c23770f6c86107c5df80656c27f9",
+    "url": "/telegram-coding-contest-charts/index.html"
+  }
+];
